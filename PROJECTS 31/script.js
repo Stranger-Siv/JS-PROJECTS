ef68e22b6599fdc40acbf4f1857e365bc1e3b528
Sivram Pradhan <programmersiv21@gmail.com>
2024-04-17 01:31:20 +0530

    changes

--- PROJECTS 31/script.js
+++ PROJECTS 31/script.js
@@ -65,7 +65,6 @@ const handleChat = () => {
 }
 
 chatInput.addEventListener("input", () => {
-    // Adjust the height of the input textarea based on its content
     chatInput.style.height = `${inputInitHeight}px`;
     chatInput.style.height = `${chatInput.scrollHeight}px`;
 });
